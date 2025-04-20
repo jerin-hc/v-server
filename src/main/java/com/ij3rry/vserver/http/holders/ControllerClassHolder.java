068f176dc38923c497f0bbcd98ff3f1485225a6d
Jerin Johnykutty <36392038+J3rryCodes@users.noreply.github.com>
2025-04-20 19:07:40 +0530

    Fixed class loading exception handling.

--- src/main/java/com/ij3rry/vserver/http/holders/ControllerClassHolder.java
+++ src/main/java/com/ij3rry/vserver/http/holders/ControllerClassHolder.java
@@ -21,33 +21,31 @@ public class ControllerClassHolder {
 
     public static void loadAllControllerClass(Map<String,Object> configs){
         clazz = new HashMap<>();
-        Map<String,Object> routes = (Map<String, Object>) configs.get("routes");
-        try {
+        Map<String, Object> routes = (Map<String, Object>) configs.get("routes");
 
-            Iterator<Map.Entry<String, Object>> methodsEntrySet = routes.entrySet().iterator();
-            while (methodsEntrySet.hasNext()){
-                Map.Entry<String, Object> methodEntry = methodsEntrySet.next();
-                String method = methodEntry.getKey();
-                List<Map<String,Object>> endpoints =(List<Map<String,Object>>) methodEntry.getValue();
+        for (Map.Entry<String, Object> methodEntry : routes.entrySet()) {
+            String method = methodEntry.getKey();
+            List<Map<String, Object>> endpoints = (List<Map<String, Object>>) methodEntry.getValue();
 
-                Map<String,Object> endpointClassMap = new HashMap<>();
-                for( Map<String,Object> endpoint : endpoints ){
-                    if(!endpoint.get("type").equals("controller")){
-                        continue;
-                    }
+            Map<String, Object> endpointClassMap = new HashMap<>();
+            for (Map<String, Object> endpoint : endpoints) {
+                if (!endpoint.get("type").equals("controller")) {
+                    continue;
+                }
+                try {
                     Class<?> clazz = Class.forName((String) endpoint.get("path"));
                     Object obj = clazz.getDeclaredConstructor().newInstance();
-                    endpointClassMap.put((String) endpoint.get("endpoint"), obj );
-                }
-                if(!endpointClassMap.isEmpty()) {
-                    clazz.put(method, endpointClassMap);
+                    endpointClassMap.put((String) endpoint.get("endpoint"), obj);
+                } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
+                         InstantiationException | IllegalAccessException e) {
+                    LOGGER.warn("Unable to load controller class : ", e);
                 }
             }
-            LOGGER.info("Controller classed are loaded {}",clazz);
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-            System.out.println("Class not found");
-        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+            if (!endpointClassMap.isEmpty()) {
+                clazz.put(method, endpointClassMap);
+            }
         }
+        LOGGER.info("Controller classed are loaded {}", clazz);
+
     }
 }
